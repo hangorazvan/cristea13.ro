6f242d1183c7ef9257f6fb5367d0f0822fca88c2
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-09-06 17:07:33 +0300

    Add files via upload

---
+++ petre/index.php
@@ -0,0 +1,6 @@
+<?php
+	header("Cache-Control: no-cache, no-store, must-revalidate, no-transform, private, max-age=0");
+	header("Pragma: no-cache");
+	header("Expires: 0");
+	include('index.html');
+?>
